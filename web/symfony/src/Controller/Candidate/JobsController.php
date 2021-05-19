4fb39179c609cf6a7c57a9aec39614217b95359f
jautu <cquintana@dokify.net>
2021-05-19 21:07:50 +0200

    Cambios en solicitudes

--- web/symfony/src/Controller/Candidate/JobsController.php
+++ web/symfony/src/Controller/Candidate/JobsController.php
@@ -2,9 +2,12 @@
 
 namespace App\Controller\Candidate;
 
+use App\Entity\Inscription;
 use App\Repository\InscriptionRepository;
 use App\Repository\JobOfferRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
 class JobsController extends AbstractController
@@ -12,9 +15,38 @@ class JobsController extends AbstractController
     public function jobs(InscriptionRepository $inscriptionRepository, JobOfferRepository $jobOfferRepository): Response
     {
         $inscriptions = $inscriptionRepository->findBy(['user' => $this->getUser()]);
-        $jobs = $jobOfferRepository->all();
+        $jobs = $jobOfferRepository->findAll();
 
         return $this->render('candidate/jobs.html.twig', [
+            'inscriptions' => $inscriptions,
+            'jobs' => $jobs,
+        ]);
+    }
+
+    public function inscription(Request $request, int $id, InscriptionRepository $inscriptionRepository, JobOfferRepository $jobOfferRepository, EntityManagerInterface $em): Response
+    {
+        try {
+            $job = $jobOfferRepository->find($id);
+
+            $inscription = new Inscription();
+            $inscription->setUser($this->getUser());
+            $inscription->setJobOffer($job);
+            $inscription->setCreationDate(new \DateTimeImmutable());
+
+            $em->persist($inscription);
+            $em->flush();
+            $message = 'Se ha realizado tu inscripción correctamente.';
+        } catch (\Exception $ex) {
+            $message = 'Ya estás inscrito en esta oferta.';
+        }
+
+        $inscriptions = $inscriptionRepository->findBy(['user' => $this->getUser()]);
+
+
+        $jobs = $jobOfferRepository->findAll();
+
+        return $this->render('candidate/search_jobs.html.twig', [
+            'message' => $message,
             'inscriptions' => $inscriptions,
             'jobs' => $jobs,
         ]);
--- web/symfony/src/Controller/Candidate/SearchJobsController.php
+++ web/symfony/src/Controller/Candidate/SearchJobsController.php
@@ -2,17 +2,20 @@
 
 namespace App\Controller\Candidate;
 
+use App\Repository\InscriptionRepository;
 use App\Repository\JobOfferRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 
 class SearchJobsController extends AbstractController
 {
-    public function search(JobOfferRepository $jobOfferRepository): Response
+    public function search(InscriptionRepository $inscriptionRepository, JobOfferRepository $jobOfferRepository): Response
     {
+        $inscriptions = $inscriptionRepository->findBy(['user' => $this->getUser()]);
         $jobs = $jobOfferRepository->findAll();
 
         return $this->render('candidate/search_jobs.html.twig', [
+            'inscriptions' => $inscriptions,
             'jobs' => $jobs
         ]);
     }
