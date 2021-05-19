19e0be63e807c3ee91658b774b5f247dd9461494
jautu <cquintana@dokify.net>
2021-05-19 19:36:00 +0200

    Añadida sección de empleo - modificación de controladores

--- web/symfony/src/Controller/Candidate/JobsController.php
+++ web/symfony/src/Controller/Candidate/JobsController.php
@@ -2,13 +2,21 @@
 
 namespace App\Controller\Candidate;
 
+use App\Repository\InscriptionRepository;
+use App\Repository\JobOfferRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 
 class JobsController extends AbstractController
 {
-    public function jobs(): Response
+    public function jobs(InscriptionRepository $inscriptionRepository, JobOfferRepository $jobOfferRepository): Response
     {
-        return $this->render('candidate/jobs.html.twig', []);
+        $inscriptions = $inscriptionRepository->findBy(['user' => $this->getUser()]);
+        $jobs = $jobOfferRepository->all();
+
+        return $this->render('candidate/jobs.html.twig', [
+            'inscriptions' => $inscriptions,
+            'jobs' => $jobs,
+        ]);
     }
 }
--- web/symfony/src/Controller/Candidate/SearchJobsController.php
+++ web/symfony/src/Controller/Candidate/SearchJobsController.php
@@ -2,15 +2,18 @@
 
 namespace App\Controller\Candidate;
 
+use App\Repository\JobOfferRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 
 class SearchJobsController extends AbstractController
 {
-    public function search(): Response
+    public function search(JobOfferRepository $jobOfferRepository): Response
     {
+        $jobs = $jobOfferRepository->findAll();
+
         return $this->render('candidate/search_jobs.html.twig', [
-            'jobs' => []
+            'jobs' => $jobs
         ]);
     }
 }
